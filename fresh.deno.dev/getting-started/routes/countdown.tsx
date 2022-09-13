130baac3ce62e2cea41a59ad48a3554710516c1b
Chase May <clmay@protonmail.com>
2022-09-13 10:08:12 -0700

    Change timer to 10s

--- fresh.deno.dev/getting-started/routes/countdown.tsx
+++ fresh.deno.dev/getting-started/routes/countdown.tsx
@@ -4,7 +4,7 @@ import Countdown from "../islands/Countdown.tsx";
 
 export default function Page() {
   const date = new Date();
-  date.setHours(date.getHours() + 1);
+  date.setSeconds(date.getSeconds() + 10);
   return (
     <p>
       The big event is happening <Countdown target={date.toISOString()} />.
