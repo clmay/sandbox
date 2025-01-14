77173cdd7907fa2665be4b28a6ac827407ce6e10
Chase May <hello@clm.dev>
2025-01-14 11:47:17 -0800

    Add new quine

--- js/quines.js
+++ js/quines.js
@@ -1,4 +1,10 @@
 // @ts-nocheck
 // prettier-ignore
 
+// Some JavaScript quines I've come up with
+
+// First attempt, 22 chars
 (q=()=>`(q=${q})()`)()
+
+// Second attempt, 21 chars
+(q=_=>`(q=${q})()`)()
